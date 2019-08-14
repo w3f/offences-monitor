afa8477b9fa26ff47863f77eb935866c1b20495a
Logan Saether <x@logansaether.com>
2019-08-14 23:55:38 +0200

    init, doesn't actually do anything exciting yet

---
+++ README.md
@@ -0,0 +1,20 @@
+# Offences Monitor
+
+Monitors slashable offences that are registered through the Offences module on a Substrate based chain.
+
+## Running
+
+Clone the repository to the desired environment, install dependencies and run via `yarn`.
+
+```zsh
+$ git clone https://github.com/w3f/offences-monitor.git
+$ cd offences-monitor
+$ yarn
+$ yarn start
+```
+
+The monitor will use the endpoint of a locally running chain at `ws://localhost:9944` by default. To point it to a different endpoint provide an argument to `yarn start` like so:
+
+```zsh
+$ yarn start wss://kusama-rpc.polkadot.io
+```
---
+++ src/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+
+// const OFFENSE_TYPES = {
+//   Kind: 0,
+//   SessionIndex: 1,
+//   TimeSlot: 2,
+// }
+
+// const Offences = {
+//   BabeEquivocation: 'babe:equivocatio',
+//   GrandpaEquivocation: 'grandpa:equivoca',
+//   Unresponsiveness: 'im-online:offlin',
+// }
+
+const Report = {};
+
+/// Express
+const app = express();
+const port = 5555;
+
+/// Polkadot API Endpoint
+const LocalEndpoint = 'ws://localhost:9944';
+
+app.get('/report', (req, res) => {
+  res.send(Report);
+});
+
+// app.get('/test', (req, res) => {
+//   Report[Offences.Unresponsiveness] = {
+//     sessionIndex: 2,
+//     timeSlot: 44,
+//   };
+// });
+
+const main = async (endpoint = LocalProvider) => {
+  console.log(`Starting events monitor watching endpoint: ${endpoint}`);
+
+  const provider = new WsProvider(endpoint);
+  const api = await ApiPromise.create({ provider });
+
+  /// Subscribe to all events...
+  api.query.system.events((events) => {
+    // ... but only filter the ones we care about -- offences.
+    events.forEach((record) => {
+      const { event } = record;
+
+      if (event.section === 'offences') {
+        const offenseKind = event.data[0];
+
+        /// Store in memory. TODO: Replace this with `prom-client`.
+        Report[offenseKind] = {
+          sessionIndex: event.data[1],
+          timeSlot: event.data[2],
+        }
+      }
+    })
+  });
+
+  app.listen(port, () => console.log(`Offences monitor running on port ${port}`));
+
+}
+
+try {
+  const endpoint = process.argv[2] ?
+    (process.argv[2].startsWith('ws://')  || process.argv[2].startsWith('wss://') ? process.argv[2]: LocalEndpoint) : LocalEndpoint;
+  main(endpoint);
+} catch (ERR) { console.error(ERR); }
